a22d32fe403c54f670202c0df4b29b59cbf31093
Lucky-PC\Lucky <klakshmisagar07@gmail.com>
2017-01-14 07:24:37 -0700

    Creating the home UI and animations

--- app/src/main/java/planyourtrip/com/planyourtrip/View/HomeActivity.java
+++ app/src/main/java/planyourtrip/com/planyourtrip/View/HomeActivity.java
@@ -1,7 +1,12 @@
 package planyourtrip.com.planyourtrip.View;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import planyourtrip.com.planyourtrip.R;
 
@@ -11,5 +16,46 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void flightTap(View view) {
+        ImageButton button = (ImageButton)findViewById(R.id.flight);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
+    }
+    public void carTap(View view) {
+        ImageButton button = (ImageButton)findViewById(R.id.car);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
+    }
+    public void trainTap(View view) {
+        ImageButton button = (ImageButton)findViewById(R.id.train);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
+    }
+    public void hotelTap(View view) {
+        ImageButton button = (ImageButton)findViewById(R.id.hotel);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
     }
 }
