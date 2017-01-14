4e70f7c0505e235d9c35af43a108fced4696ab99
nithesh1990 <nithesh1990@gmail.com>
2017-01-14 04:35:57 -0700

    Commit 1 - Created Project and Home Activity

---
+++ app/src/main/java/planyourtrip/com/planyourtrip/HomeActivity.java
@@ -0,0 +1,13 @@
+package planyourtrip.com.planyourtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
